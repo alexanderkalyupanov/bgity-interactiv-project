7ceb1db377f1cd4a68626d2368ca55178bb35209
Alexander Kalyupanov <alexander.kalyupanov@mail.ru>
2025-02-06 14:36:56 +0300

    feat: basic version nav menu

--- project-bgity/assets/js/main.js
+++ project-bgity/assets/js/main.js
@@ -7,13 +7,13 @@ function hamburgerMenu() {
     hamburgerBtn.addEventListener("click", () => {
         bodyWrapper.classList.toggle("body-overflow")
         hamburgerBtn.classList.toggle("active");
-        navList.classList.toggle("nav-active");
+        navList.classList.toggle("active");
     })
     
     navList.addEventListener("click", (e) => {
         if (e.target.classList.contains("nav-list__link")) {
             bodyWrapper.classList.remove("body-overflow")
-            navList.classList.remove("nav-active");
+            navList.classList.remove("active");
             hamburgerBtn.classList.remove("active");
         }
     })
