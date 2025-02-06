3b14588a086179e54e08ba6b445d2b7888967f20
alexanderkalyupanov <138789735+alexanderkalyupanov@users.noreply.github.com>
2025-02-06 10:30:56 +0300

    work on burger menu

--- project-bgity/assets/js/main.js
+++ project-bgity/assets/js/main.js
@@ -7,13 +7,13 @@ function hamburgerMenu() {
     hamburgerBtn.addEventListener("click", () => {
         bodyWrapper.classList.toggle("body-overflow")
         hamburgerBtn.classList.toggle("active");
-        navList.classList.toggle("active");
+        navList.classList.toggle("nav-active");
     })
     
     navList.addEventListener("click", (e) => {
         if (e.target.classList.contains("nav-list__link")) {
             bodyWrapper.classList.remove("body-overflow")
-            navList.classList.remove("active");
+            navList.classList.remove("nav-active");
             hamburgerBtn.classList.remove("active");
         }
     })
